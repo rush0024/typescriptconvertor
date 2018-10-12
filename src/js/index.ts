3256846d1bdb4aba54c8259b58e723cbdd00dda4
Rushika Tamrakar <Rushika Tamrakar@LAPTOP-D490EOAI>
2018-10-12 10:07:29 +0200

    new mandatory assignment
    changes

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,28 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+let InputData: HTMLInputElement = <HTMLInputElement>document.getElementById("InputData");
+let ToGramsButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("ToGramsButton");
+
+let Output: HTMLSpanElement = <HTMLSpanElement>document.getElementById("output");
+
+
+function ConvertToGrams(): void
+{
+    let number1string: string = InputData.value;
+    let number1number = Number(number1string);
+    let result : number = number1number * 28.3495231;
+    Output.innerHTML = result.toString()
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+ToGramsButton.addEventListener("click", ConvertToGrams);
+
+function ConvertToOz(): void{
+    let number1string: string = InputData.value;
+    let number1number = Number(number1string);
+    let result : number = number1number * 0.0352739619;
+    Output.innerHTML = result.toString()
+    
+}
+
+let ToOzButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("ToOzButton");
+ ToOzButton.addEventListener("click", ConvertToOz);
+
